99e0a9fe02407463c96179d10cbfed68aafb7966
mr7kitkat <mr7kitkat@gmail.com>
2024-11-18 10:07:58 +0530

    make some frontend changes

--- scripts/main.js
+++ scripts/main.js
@@ -3,53 +3,128 @@ const outputPanel = document.querySelector(".output");
 const expressionScreen = outputPanel.querySelector(".expression");
 const resultScreen = outputPanel.querySelector(".result");
 const btns = document.querySelectorAll(".getvalue");
-const deleteBtn = document.querySelector("#deletebtn");
+const clearbtn = document.querySelector("#clearbtn");
+const allclearbtn = document.querySelector("#allclearbtn");
+const openingBracket = document.querySelector("#openingBracket");
+const closingBracket = document.querySelector("#closingBracket");
 
 // THE EXPRESSION ARRAY
-const EXPRESSION = [];
-let tempNumHolder = "";
+let EXPRESSION = [0];
+
+// SETTING INITIAL VALUES
+function setInitialValues() {
+  EXPRESSION = [0];
+  expressionScreen.innerText = 0;
+  closingBracket.dataset.balance = 0;
+}
 
 // for every button input
 btns.forEach((btn) => {
   btn.addEventListener("click", function (e) {
     const currentInput = e.target.dataset.value;
-    const validationResult = runValidations(EXPRESSION, currentInput);
-    if (validationResult) {
-      expressionScreen.innerText += validationResult;
+    const validationRes = checkExpressionInput(EXPRESSION, currentInput);
+    if (validationRes) {
+      renderArray();
     }
   });
 });
 
-// delete btn work
-deleteBtn.addEventListener("click", function () {
-  let currentValue = expressionScreen.innerText;
-  expressionScreen.innerText = currentValue.slice(0, currentValue.length - 1);
-  console.log(EXPRESSION);
+// openingBracket eventlistner
+openingBracket.addEventListener("click", function () {
+  closingBracket.dataset.value = ")";
+  closingBracket.dataset.balance = +closingBracket.dataset.balance + 1;
 });
 
-// =============== Helper function ===============
-// run validation checks
-function checkValidation(expressionArray, input) {
-  // 1. check if the input is valid input for calculator
-  const validSymbols = /[0-9+\-/×\/^√]/;
-  if (!validSymbols.test(input)) {
-    // if symbol is not a valid calculator symbol then return false
-    return false;
+// closingBracket eventlistner
+closingBracket.addEventListener("click", function () {
+  if (+closingBracket.dataset.balance > 0) {
+    closingBracket.dataset.value = ")";
+    closingBracket.dataset.balance = +closingBracket.dataset.balance - 1;
   }
 
-  // if expression has items in it then check if last digit is symbol then
-  // currentInput must be a number
-  const len = expressionArray.length;
-  const symbolRegEx = /[+\-/×\/^√]/;
-  if (len > 0) {
-    const lastItem = expressionArray.slice(-1);
-    if (symbolRegEx.test(input) && symbolRegEx.test(lastItem)) {
-      return false;
+  if (+closingBracket.dataset.balance <= 0) {
+    closingBracket.dataset.value = "";
+  }
+});
+
+// clear btn work (single character)
+clearbtn.addEventListener("click", function () {
+  EXPRESSION.pop();
+  renderArray();
+});
+
+// all clear btn work
+allclearbtn.addEventListener("click", function () {
+  setInitialValues();
+});
+
+// run validations
+function checkExpressionInput(expAry, currentInput) {
+  const currentLength = () => expAry.length;
+  // this line is taken from chatgpt
+  let validRegEx = /^(?:[0-9.\-+×\/%()^√]|(?:sin|cos|tan|log|e|√|π))+$/i;
+
+  const runTest = (validRegEx, item = currentInput) => validRegEx.test(item);
+  // check if currentInput is valid
+  if (runTest(validRegEx)) {
+    if (currentLength() > 1) {
+      const lastItem = () => expAry.slice(-1)[0];
+
+      // if last item is - + x / operator and currentInput is also one of them
+      // then change currentInput to the last input
+      validRegEx = /[\-+×\/]/;
+      if (runTest(validRegEx) && runTest(validRegEx, lastItem())) {
+        expAry[expAry.length - 1] = currentInput;
+        return true;
+      }
+
+      // if last item is a digit then next valid items are
+      //   + - * / % ^ . 0 to 9 , )
+      // invalid: sin cos tan log 1/x e sqrt (
+      validRegEx = /[0-9.\-+×\/%)^]/;
+      if (runTest(/[0-9]/, lastItem())) {
+        console.log(runTest(validRegEx));
+
+        if (runTest(validRegEx)) {
+          expAry.push(currentInput);
+
+          return true;
+        } else {
+          
+          currentInput = `×${currentInput}(`;
+          expAry.push(currentInput);
+          return true;
+        }
+      }
+    } else {
+      // if expression 1st item is 0
+      if (expAry[0] == 0) {
+        validRegEx = /[0-9(]/;
+        if (runTest(validRegEx)) {
+          expAry[0] = currentInput;
+          return true;
+        }
+      }
+      // if expression 1st item is other then 0
+      else {
+        validRegEx = /[0-9.\-+×\/%^]/;
+        if (runTest(validRegEx)) {
+          expAry.push(currentInput);
+          return true;
+        } else {
+          currentInput = `×${currentInput}(`;
+          expAry.push(currentInput);
+          return true;
+        }
+      }
     }
   }
-  // if expression has no item in it just check if the first input is number
-  // if it is number then ok other wise return false
-  else if (symbolRegEx.test(input) && len == 0) {
-    return false;
-  }
+
+  // if it is not a valid input just return false to stop input
+  return false;
+}
+
+// calculator program
+function renderArray(expAry = EXPRESSION) {
+  expressionScreen.innerText = expAry.join("");
 }
