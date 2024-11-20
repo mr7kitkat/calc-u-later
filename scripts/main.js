f605e0370dbe5dad50787db3799f477dda6cfd2d
mr7kitkat <mr7kitkat@gmail.com>
2024-11-20 09:26:41 +0530

    algorithm is working, but there is slightly an issue

--- scripts/main.js
+++ scripts/main.js
@@ -10,112 +10,180 @@ const closingBracket = document.querySelector("#closingBracket");
 
 // the calculator Object
 const calc = {
-    exp: [],
+  exp: [],
 
-    // Get the last item of the exp array
-    get lastItem() {
-        if (this.length) {
-            return this.exp.slice(-1);
-        }
-    },
-
-    // get the length of the array
-    get length() {
-        return this.exp.length;
-    },
-
-    // add items to the array
-    add: function (item) {
-        const validationCheck = /\b(?:[0-9.+\-×\/^√π%()]|e|sin|cos|tan|log)\b/;
-        if (validationCheck.test(item)) {
-            const validations = {
-                // its for number 0 to 9
-                number: {
-                    prevChecks: /\b(?:[0-9.+\-×\/√^(])\b/,
-                    afterChecks: /\b(?:[0-9.+\-×\/%^)])\b/,
-                    withDefaultPrefix: /\b(?:[√π(]|e|sin|cos|tan|log)\b/,
-                    defaultPrefix: "×"
-                },
-                // it is for the basic math basicOperator
-                // +-×/
-                basicOperator: {
-                    prevChecks: /\b(?:[0-9.π%)]|e|sin|cos|tan|log)\b/,
-                    afterChecks: /\b(?:[0-9√π(]|e|sin|cos|tan|log)\b/,
-                    withDefaultPrefix,
-                    defaultPrefix
-                },
-                //validations for percentage %
-                percentage: {
-                    prevChecks: /\b(?:[0-9π]|e)\b/,
-                    afterChecks: /[+\-×\/^]/,
-                    withDefaultPrefix: /\b(?:[0-9√π(]|e|sin|cos|tan|log)\b/,
-                    defaultPrefix: "×"
-                },
-                // sqrt
-                sqrt: {
-                    prevChecks: /[+\-×\/√^(]/,
-                    afterChecks: /\b(?:[0-9√π(]|e|sin|cos|tan|log)\b/,
-                    withDefaultPrefix,
-                    defaultPrefix
-                },
-                // square
-                square: {
-                    prevChecks: /\b(?:[0-9.π%)]|e)\b/,
-                    afterChecks: /\b(?:[0-9\-√π(]|e|sin|cos|tan|log)\b/,
-                    withDefaultPrefix,
-                    defaultPrefix
-                },
-                // scientific function
-                scientificFunctions: {
-                    prevChecks: /[+\-×\/√^(]/,
-                    afterChecks: /[(]/,
-                    withDefaultPrefix: /\b(?:[0-9√π(]|e|sin|cos|tan|log)\b/,
-                    defaultPrefix: "("
-                },
-                dot: {
-                    prevChecks: /[0-9]/,
-                    afterChecks: /[0-9+\-×\/^)]/,
-                    withDefaultPrefix: /\b(?:[√π(]|e|sin|cos|tan|log)\b/,
-                    defaultPrefix: "×"
-                }
-            };
-
-            if (this.length == 0) {
-                if (/\b(?:[0-9√π(]|e|sin|cos|tan|log)\b/.test(item)) {
-                    this.exp.push(item);
-                    this.renderCalc();
-                }
-            } else {
-                function validationFunc(objectOfLastItem, input) {
-                    const {
-                        prevChecks,
-                        afterChecks,
-                        withDefaultPrefix,
-                        defaultPrefix
-                    } = objectOfLastItem;
-                    
-                    if (afterChecks.test(input)) {
-                      this.exp.push(input)
-                    }
-                    else if (withDefaultPrefix.test(input)) {
-                      const item = defaultPrefix + input;
-                      this.exp.push(input)
-                    }
-                    
-                    
-                }
-            }
-        }
-    },
-
-    // remove items from the array
-    get remove() {
-        this.exp.pop();
-    },
-
-    // render expression to the screen
-    renderCalc: function (nodename) {
-        nodename.innerText = this.exp.join("");
-        console.log(this.exp);
+  // Get the last item of the exp array
+  get lastItem() {
+    if (this.length) {
+      return this.exp.slice(-1)[0];
     }
+  },
+
+  // get the length of the array
+  get length() {
+    return this.exp.length;
+  },
+
+  // add items to the array
+  add: function (item) {
+    // gate of validaṁtions
+    console.log("CLICKED - ", item);
+    const validationCheck = /[0-9\+\-\×\%\/\(\)\^\.\√\π\s\e\sin\cos\tan\log]/gi;
+
+    if (validationCheck.test(item)) {
+      const typeValidation = {
+        // its for number 0 to 9
+        number: {
+          prevChecks: /[0-9\.\+\-\×\/\√\^\(]/gi,
+          afterChecks: /[0-9\.\+\-\×\/\%\^\)]/gi,
+          withDefaultPrefix: /[\√\π\(\e\sin\cos\tan\log]/gi,
+          defaultPrefix: "×",
+        },
+        // it is for the basic math basicOperator
+        // +-×/
+        basicOperator: {
+          prevChecks: /[0-9\.\π\%\)\e\sin\cos\tan\log]/gi,
+          afterChecks: /[0-9\√\π\(\e\sin\cos\tan\log]/gi,
+          withDefaultPrefix: null,
+          defaultPrefix: null,
+        },
+        //validations for percentage %
+        percentage: {
+          prevChecks: /[0-9\π\e]/gi,
+          afterChecks: /[\+\-\×\/\^]/gi,
+          withDefaultPrefix: /[0-9\√\π\(\e\sin\cos\tan\log]/gi,
+          defaultPrefix: "×",
+        },
+        // sqrt
+        sqrt: {
+          prevChecks: /[\+\-\×\/\√\^\(]/gi,
+          afterChecks: /[0-9\√\π\(\e\sin\cos\tan\log]/gi,
+          withDefaultPrefix: null,
+          defaultPrefix: null,
+        },
+        // square
+        square: {
+          prevChecks: /[0-9\.\π\%\)\e]/gi,
+          afterChecks: /[0-9\-\√\π\(\e\sin\cos\tan\log]/gi,
+          withDefaultPrefix: null,
+          defaultPrefix: null,
+        },
+        // scientific function
+        scientificFunctions: {
+          prevChecks: /[\+\-\×\/\√\^\(]/gi,
+          afterChecks: /[\(]/gi,
+          withDefaultPrefix: /[0-9\√\π\(\e\sin\cos\tan\log]/gi,
+          defaultPrefix: "(",
+        },
+        dot: {
+          prevChecks: /[0-9]/gi,
+          afterChecks: /[0-9\+\-\×\/\^\)]/gi,
+          withDefaultPrefix: /[\√\π\(\e\sin\cos\tan\log]/gi,
+          defaultPrefix: "×",
+        },
+        pi: {
+          prevChecks: /[\+\-\×\/\^\√\(]/gi,
+          afterChecks: /[\+\-\×\/\^\%\)]/gi,
+          withDefaultPrefix: /[0-9\√\π\(\e\sin\cos\tan\log]/gi,
+          defaultPrefix: "×",
+        },
+      };
+
+      if (this.length <= 0) {
+        // if there is nothing in expression then check for number and scientific inputs
+        if (/[0-9\√\π\(\e\sin\cos\tan\log]/gi.test(item)) {
+          let itemToPush = item;
+          if (/[\sin\cos\tan\log]/gi.test(item)) {
+            itemToPush += "(";
+          }
+          this.exp.push(itemToPush);
+        }
+      }
+      // if there are more then one item in expression
+      // check for validations depending on type
+      else {
+        // this function takes object of previous item and current input
+        const validateAndMakeEntry = (objectOfLastItem, input) => {
+          const { prevChecks, afterChecks, withDefaultPrefix, defaultPrefix } =
+            objectOfLastItem;
+
+          if (afterChecks.test(input)) {
+            if (/[\sin\cos\tan\log]/gi.test(input)) {
+              return input + "(";
+            }
+            return input;
+          } else {
+            if (withDefaultPrefix) {
+              if (withDefaultPrefix.test(input)) {
+                let theItemToPush = input;
+                if (/[\sin\cos\tan\log]/gi.test(input)) {
+                  theItemToPush += "(";
+                }
+                return defaultPrefix + theItemToPush;
+              }
+            }
+          }
+
+          return false;
+        };
+
+        const lastInput = this.lastItem;
+        const {
+          number,
+          basicOperator,
+          dot,
+          scientificFunctions,
+          sqrt,
+          square,
+          percentage,
+          pi,
+        } = typeValidation;
+
+        let entry = "";
+        if (/[0-9]/gi.test(lastInput)) {
+          entry = validateAndMakeEntry(number, item);
+        } else if (/[\.]/gi.test(lastInput)) {
+          entry = validateAndMakeEntry(dot, item);
+        } else if (/[\+\-\×\/]/gi.test(lastInput)) {
+          entry = validateAndMakeEntry(basicOperator, item);
+        } else if (/[\%]/gi.test(lastInput)) {
+          entry = validateAndMakeEntry(percentage, item);
+        } else if (/[\^]/gi.test(lastInput)) {
+          entry = validateAndMakeEntry(square, item);
+        } else if (/[\√]/gi.test(lastInput)) {
+          entry = validateAndMakeEntry(sqrt, item);
+        } else if (/[\e\sin\cos\tan\log]/gi.test(lastInput)) {
+          entry = validateAndMakeEntry(
+            scientificFunctions,
+            item,
+            this.exp,
+            "scientificFunctions"
+          );
+        } else if (/[\π]/gi.test(lastInput)) {
+          entry = validateAndMakeEntry(pi, item);
+        }
+
+        if (entry) {
+          this.exp.push(entry);
+        }
+      }
+    }
+  },
+  // remove items from the array
+  get remove() {
+    this.exp.pop();
+  },
+
+  // render expression to the screen
+  renderCalc(nodename) {
+    nodename.innerText = this.exp.join("");
+  },
 };
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    const targetbtn = e.target.dataset.value;
+    calc.add(targetbtn);
+    calc.renderCalc(expressionScreen);
+  });
+});
